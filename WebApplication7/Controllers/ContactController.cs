c407c5960b20bff81bb9504fc6e7796a298301db
ivo-95 <ivo145@abv.bg>
2015-08-06 13:01:24 +0300

    Added contact form functionality.

--- WebApplication7/Controllers/ContactController.cs
+++ WebApplication7/Controllers/ContactController.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
+using WebApplication7.Models;
 
 namespace WebApplication7.Controllers
 {
@@ -12,6 +16,42 @@ namespace WebApplication7.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Submit(EmailFormModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    SmtpClient smtp = new SmtpClient();
+                    smtp.Host = "smtp.gmail.com";
+                    smtp.Port = 25;
+                    smtp.EnableSsl = true;
+                    smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
+                    smtp.UseDefaultCredentials = false;
+                    smtp.Credentials = new NetworkCredential("carshopasp@gmail.com", "aspcarshop");
+
+                    using (var message = new MailMessage(model.FromEmail, "carshopasp@gmail.com"))
+                    {
+                        message.Subject = "Message from " + model.FromName + "; E-mail: " + model.FromEmail;
+                        message.Body = model.Message;
+                        message.IsBodyHtml = true;
+                        smtp.Send(message);
+                    }
+
+                    ViewBag.message = "Thank you for your feedback, " + model.FromName + "! Your message was sent successfully.";
+                    return View("Index");
+                }
+                catch (Exception)
+                {
+                    ViewBag.message = "We are sorry, " + model.FromName + ". Something went wrong.";
+                    return View("Index");
+                }
+            }
+            return View("Index");
         }
     }
 }
---
+++ WebApplication7/Models/EmailFormModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication7.Models
+{
+    public class EmailFormModel
+    {
+        [Required(ErrorMessage = "Your name is required!")]
+        public string FromName { get; set; }
+
+        [Required(ErrorMessage = "Your e-mail is required!")]
+        [EmailAddress(ErrorMessage = "This is not a valid e-mail address!")]
+        public string FromEmail { get; set; }
+
+        [Required(ErrorMessage = "Please enter a comment!")]
+        public string Message { get; set; }
+    }
+}
